a135daafcd89b5d8ed550b175ad596848f490e84
Alexandr <skainetd@gmail.com>
2016-08-10 20:35:23 +0300

    test mobile menu

--- js/common.js
+++ js/common.js
@@ -1,3 +1,25 @@
+function menu_click()
+	{
+		$(document).ready(function() 
+		{
+			var navigation = $(".navigation").css("display");
+			if (navigation == "none") 
+			{
+				$(".navigation").css("display", "block");
+				$(".navigation").css("position", "static");	
+				$(".navigation").css("text-align", "center");				
+				$(".navigation").css("background-color", "#0d0d0d");	
+			}
+			else
+			{
+				$(".navigation").css("display", "none");
+				$(".navigation").css("position", "absolute");	
+				$(".navigation").css("text-align", "left");				
+				$(".navigation").css("background-color", "none");
+			}
+		});
+	}
+
 $(document).ready(function() 
 {
 	function fullsizewindow()
@@ -23,6 +45,7 @@ $(document).ready(function()
 	fullsizewindow();
 	$(window).resize(function()
 	{
+		
 		fullsizewindow();
 	})
 
