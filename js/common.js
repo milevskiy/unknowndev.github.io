1a81bb5a29de4395f036d3ee0e224c8b94c60366
Alexandr Milevskiy <skainetd@gmail.com>
2016-11-22 18:55:26 +0300

    updated skills and desc

--- js/common.js
+++ js/common.js
@@ -28,21 +28,6 @@ $(document).ready(function()
 		$(".parallax-window").css("height", $(window).height());
 	}
 
-	function skillcalculate(name, count)
-	{
-		var i = 1;
-		$(name).each(function(){
-			if (i <= count) $(this).css("background-color", "#20bc99");
-			i++;
-     	});
-	}
-
-	skillcalculate(".csharp td", 7);
-	skillcalculate(".javascript td", 5);
-	skillcalculate(".unity td", 7);
-	skillcalculate(".htmlcss td", 8);
-	skillcalculate(".aspnet td", 1);
-
 	fullsizewindow();
 	$(window).resize(function()
 	{
